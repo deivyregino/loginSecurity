f4a4ec13a4ed941d683d58be9e09d477752936e4
DEIVY JOSE REGINO TORRES <deivy.reginot@gmail.com>
2019-11-20 14:11:18 -0500

    Correcion de Metodo Registrar y Encriptacion
    Se corrigio en la clase registrarUsuario el metodo para registrar y se hizo un llamado del metodo para encriptar el Email y el Password. Se debe revisar ingresando varios registros.

--- app/src/main/java/com/example/loginsegurity/MainActivity.java
+++ app/src/main/java/com/example/loginsegurity/MainActivity.java
@@ -3,11 +3,53 @@ package com.example.loginsegurity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 public class MainActivity extends AppCompatActivity {
+
+    static String clave = "deivy";
+    static SecretKeySpec secret;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Metodo para generar clave
+    public static SecretKey generateKey() throws NoSuchAlgorithmException, InvalidKeyException{
+    return secret = new SecretKeySpec(clave.getBytes(), "AES");
+    }
+
+    // Metodo para encriptar
+    public static byte[] encryptMsg(String message, SecretKey secret) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, InvalidParameterException,
+            IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, secret);
+        byte[] cipherText = cipher.doFinal(message.getBytes("UTF-8"));
+        return cipherText;
+    }
+
+    // Metodo para desencriptar
+    public static String decryptMsg(byte[] cipherText , SecretKey secret) throws NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeyException, InvalidParameterException,
+            IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
+        Cipher cipher = null;
+        cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secret);
+        String  decryptString = new String(cipher.doFinal(cipherText), "UTF-8");
+        return decryptString;
     }
 }
--- app/src/main/java/com/example/loginsegurity/registroUsuario.java
+++ app/src/main/java/com/example/loginsegurity/registroUsuario.java
@@ -1,31 +1,134 @@
 package com.example.loginsegurity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.UnsupportedEncodingException;
+import java.net.PasswordAuthentication;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
 
 public class registroUsuario extends AppCompatActivity {
 
     Button btnRegistroRegistrar, btnLoginRegistrar;
+    EditText editEmail;
+
+    private EditText claveUno, claveDos, email;
+
+
+    private String emailRegistro = "" , guardarClaveUno = "", guardarClaveDos = "";
+
+
+    FirebaseAuth myAuth;
+    DatabaseReference database;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
 
-        btnLoginRegistrar = (Button) findViewById(R.id.btnLoginRegistrar);
-        btnRegistroRegistrar = (Button) findViewById(R.id.btnRegistroRegistrar);
+        myAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance().getReference();
+        email = (EditText) findViewById(R.id.emailRegistro);
+        claveUno = (EditText) findViewById(R.id.claveRegistro);
+        claveDos = (EditText) findViewById(R.id.claveRegistroConfirmar);
 
-        // Boton Login de la clase Registrar.
-
-        btnLoginRegistrar.setOnClickListener(new View.OnClickListener() {
+        btnRegistroRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(registroUsuario.this, loginUsuario.class);
-                startActivity(i);
+
+
+                emailRegistro = email.getText().toString();
+
+                guardarClaveUno = claveUno.getText().toString();
+
+                guardarClaveDos = claveDos.getText().toString();
+
+                if (emailRegistro.isEmpty() && guardarClaveUno.isEmpty() && guardarClaveDos.isEmpty() && (guardarClaveUno == guardarClaveDos)) {
+
+
+                        MainActivity ob = new MainActivity();
+                        try {
+
+                            ob.encryptMsg(guardarClaveUno, ob.secret);
+                            ob.encryptMsg(emailRegistro, ob.secret);
+
+                        } catch (NoSuchAlgorithmException e) {
+                            e.printStackTrace();
+                        } catch (NoSuchPaddingException e) {
+                            e.printStackTrace();
+                        } catch (InvalidKeyException e) {
+                            e.printStackTrace();
+                        } catch (IllegalBlockSizeException e) {
+                            e.printStackTrace();
+                        } catch (BadPaddingException e) {
+                            e.printStackTrace();
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
+                        }
+                } else {
+                    Toast.makeText(registroUsuario.this, "Por favor, llene todos los campos o verifique que las claves sean iguales", Toast.LENGTH_LONG).show();
+                }
+                registrarUsuario();
+
+
+
             }
         });
     }
+
+    private void registrarUsuario(){
+
+        myAuth.createUserWithEmailAndPassword(emailRegistro, guardarClaveUno).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+
+                    Map<String, Object> map = new HashMap<>();
+
+                    map.put("Email", email);
+                    map.put("Password", guardarClaveUno);
+
+                    String id = myAuth.getCurrentUser().getUid();
+
+                    database.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task2) {
+                            if (task2.isSuccessful()){
+                                startActivity(new Intent(registroUsuario.this, loginUsuario.class));
+                                finish();
+                            }else{
+                                Toast.makeText(registroUsuario.this, "No se pudieron crear los datos correctamente", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                }else {
+                    Toast.makeText(registroUsuario.this, "No se pudo registrar este usuario", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 }
