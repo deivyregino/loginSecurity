6eab1e897b43da7ef74b21fdbfa87132c0af9313
DEIVY JOSE REGINO TORRES <deivy.reginot@gmail.com>
2019-11-20 15:45:44 -0500

    Se completo la clase Login, Se añadio el metodo y se hizo una correcion en Registro. Importante probar, equipo con escasos recursos

--- app/src/main/java/com/example/loginsegurity/loginUsuario.java
+++ app/src/main/java/com/example/loginsegurity/loginUsuario.java
@@ -2,16 +2,27 @@ package com.example.loginsegurity;
 
 import android.content.Intent;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginUsuario extends AppCompatActivity {
 
     Button btnIniciarLogin, btnRegistrarLogin;
-    EditText emailLogin, registroLogin;
+    EditText emailLogin, claveLogin;
+
+    private String email = "", password = "";
+
+    private FirebaseAuth myAuth;
 
 
     @Override
@@ -19,10 +30,12 @@ public class loginUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_usuario);
 
+        myAuth = FirebaseAuth.getInstance();
+
         btnIniciarLogin = (Button) findViewById(R.id.btnIniciarLogin);
         btnRegistrarLogin = (Button) findViewById(R.id.btnRegistrarLogin);
         emailLogin = (EditText) findViewById(R.id.emailLogin);
-        registroLogin = (EditText) findViewById(R.id.claveLogin);
+        claveLogin = (EditText) findViewById(R.id.claveLogin);
 
 
         // Boton Registrar de la interfaz "loginUsuario"
@@ -34,6 +47,40 @@ public class loginUsuario extends AppCompatActivity {
             }
         });
 
+        btnIniciarLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = emailLogin.getText().toString();
+                password = claveLogin.getText().toString();
+
+                if (email.isEmpty() && password.isEmpty()){
+
+                    loginUsuario();
+
+                }else{
+                    Toast.makeText(loginUsuario.this, "Complete los campos", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+    }
+
+    private void loginUsuario(){
+
+        myAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    startActivity(new Intent(loginUsuario.this, homeUsuario.class));
+                    finish();
+                }else {
+                    Toast.makeText(loginUsuario.this, "No se pudo iniciar sesion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
